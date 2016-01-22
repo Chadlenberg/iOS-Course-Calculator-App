0de26a33fbfc87ec101c9fc056792fdf463cff85
Chad Wollenberg <chad.wollenberg@me.com>
2016-01-22 12:56:26 -0600

    Initial Commit

---
+++ Calculator learn to code classTests/Calculator_learn_to_code_classTests.swift
@@ -0,0 +1,36 @@
+//
+//  Calculator_learn_to_code_classTests.swift
+//  Calculator learn to code classTests
+//
+//  Created by Chad Wollenberg on 1/22/16.
+//  Copyright © 2016 Chad Wollenberg. All rights reserved.
+//
+
+import XCTest
+@testable import Calculator_learn_to_code_class
+
+class Calculator_learn_to_code_classTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
